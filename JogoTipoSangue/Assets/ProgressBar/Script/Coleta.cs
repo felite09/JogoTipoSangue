fc1e5811596ef16d2ca4c157bd37ae0aa1179f66
Felphz <felipejoliveira2@gmail.com>
2019-08-22 17:27:30 -0300

    mudanças feitas

---
+++ JogoTipoSangue/Assets/ProgressBar/Script/Coleta.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coleta : MonoBehaviour
+
+{
+
+    public static Coleta instancia;
+    GameObject vidaPer;
+    Collider2D coliderPer;
+   [SerializeField] Collider2D[] sangue;
+
+
+
+    private void Awake()
+    {
+        
+        if(instancia == null)
+        {
+            instancia = this;
+
+        }
+        else if (this != instancia)
+        {
+            Destroy(this);
+        }
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+}
